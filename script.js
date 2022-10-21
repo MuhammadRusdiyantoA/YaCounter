524d02599763c34fd6d6c605f08073bc7b580390
Muhammad Rusdiyanto Asatman <92065451+MuhammadRusdiyantoA@users.noreply.github.com>
2022-10-21 07:35:41 +0700

    Release version 2, It is now mobile compatible

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
 const counter = document.getElementById('count')
+const button = document.getElementById('reset')
+const text = document.getElementsByTagName('p')
+
+button.onclick = () => {
+    counter.innerText = -1
+}
+
+window.onclick = () => {
+    counter.innerText = Number(counter.innerText) + 1
+}
 
 document.onkeydown = (e) => {
     e = e || window.Event
@@ -7,4 +17,18 @@ document.onkeydown = (e) => {
         let count = Number(counter.innerText) + 1
         counter.innerText = count
     }
+
+    else if (e.ctrlKey && e.key === "z") {
+        counter.innerText = 0
+    }
+
+    else if (e.key === "z") {
+        if (counter.innerText > 0) {
+            counter.innerText = Number(counter.innerText) - 1
+        }
+    }
+}
+
+if (navigator.userAgent.match(/Android/i) || navigator.userAgent.match(/iPhone/i)) {
+    text.innerText = "Klik layar untuk tambah counter"
 }
